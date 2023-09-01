73857a684618e5dd669778da108d7a715365026d
Mukesh <pmukesh2701@gmail.com>
2023-09-01 11:23:16 +0530

    subtraction added

--- python/arith.py
+++ python/arith.py
@@ -7,8 +7,12 @@ def mul(n1,n2):
 def div(n1,n2):
     return(n1/n2)
 
+def sub(n1,n2):
+    return(n1-n2)   
+
 a=int(input("Enter num1:"))
 b=int(input("Enter num2:"))
 print("Sum-",add(a,b))
 print("product-",mul(a,b))
 print("quotient-",div(a,b))
+print("Remainder-",sub(a,b))
